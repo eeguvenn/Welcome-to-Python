957b687fd3867990a8eb8e2346a395ecf658c739
emine_guven <eguven.egv@gmail.com>
2025-10-25 22:35:05 +0300

    Fix input prompts and add Armstrong number controller implementation

--- algorithm_workspace.py
+++ algorithm_workspace.py
@@ -1,8 +1,8 @@
 # Perfect number controller
-
-n = int(input("Entry a number: "))
+"""
+n = int(input("Enter a number: "))
 while n <= 1:
-    n = int(input("Entry a number greater than 1: "))
+    n = int(input("Enter a number greater than 1: "))
 print("Correct number")
 
 i = 1
@@ -19,3 +19,42 @@ if m == n:
     print("Perfect Number")
 else:
     print("Not Perfect Number")
+"""
+# Armstrong number controller
+
+number = int(input("Enter a number (bigger than 0): "))
+
+count = 0
+_number = number
+buffer = []
+print(type(_number))
+
+if number <= 0:
+    print("Try again!")
+
+else:
+    while _number > 0:
+        _number = int(_number / 10)
+        print(_number)
+        x = _number * 10
+        print(x)
+        x = int((number/10**count)) - x
+        print(x)
+        buffer.append(x)
+        print(buffer)
+        count = count + 1
+        print("_number {}, count {}".format(_number, count))
+
+    print("Digit number is: ", count)
+
+    total = 0
+
+    for i in range(count):
+
+        total = total + buffer[i]**count
+        print(total)
+
+    if total == number:
+        print("Armstrong Number")
+    else:
+        print("Not Armstrong Number")
